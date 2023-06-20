f3caf35cd024edffd857d2c500301ad86b2e273a
L22390460 <L22390460@chetumal.tecnm.mx>
2023-06-20 08:58:18 -0500

    se agrega proyecto

---
+++ src/Aviones.java
@@ -0,0 +1,14 @@
+import java.time.localTime;
+import java.util.ArrayList;
+import java.util.list;
+public class Aviones {
+    String modelo;
+    public Aviones(String Modelo){
+        this.modelo= Modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+}
---
+++ src/ControldeVuelos.java
@@ -0,0 +1,5 @@
+import java.time.localTime;
+import java.util.ArrayList;
+import java.util.list;
+public class ControldeVuelos {
+}
