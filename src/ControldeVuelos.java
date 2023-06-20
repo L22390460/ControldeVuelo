89d0e482214ee20a5f274d97e8cf3a132c3dff3b
L22390460 <L22390460@chetumal.tecnm.mx>
2023-06-20 11:07:05 -0500

    actualización

--- src/ControldeVuelos.java
+++
@@ -1,5 +0,0 @@
-import java.time.localTime;
-import java.util.ArrayList;
-import java.util.list;
-public class ControldeVuelos {
-}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import java.time.localTime;
-import java.util.ArrayList;
-import java.util.list;
 public class Main {
     public static void main(String[] args) {
-
     }
 }
